7ee7b4a0c3d3be81f4a552472d66c24d7fc11481
yuki.pro <yuki.kr@gmail.com>
2017-07-28 15:29:59 +0900

    1

--- UserSetting/ViewController.swift
+++ UserSetting/ViewController.swift
@@ -10,15 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let topW = "word_03"
+    let bottomW = "word_05"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let path = Bundle.main.path(forResource: "words", ofType: "json") else { return }
+        //print(path ?? "Not a real path")
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            
+            if let dictionary = json as? [String: Any] {
+ 
+                if let nestedDictionary = dictionary["data"] as? [String: [String]] {
+                    
+                    guard let firstWordList = nestedDictionary[topW] else {return}
+                    guard let secondWordList = nestedDictionary[bottomW] else {return}
+                    
+                    var firstOldList = firstWordList
+                    var secondOldList = secondWordList
+                    print(firstOldList)
+                    print(secondOldList)
+                    
+                    
+                    // firstNewList 새로운 리스트 만들기
+                    var firstLength = firstOldList.count
+                    var firstNewList : [String] = []
+                    
+                    for _ in 0..<firstLength {
+                        let randomNumber = Int(arc4random_uniform(UInt32(firstLength)))
+                        firstNewList.append(firstOldList[randomNumber])
+                        firstOldList.remove(at: randomNumber)
+                        firstLength -= 1
+                    }
+                    
+                    // secondNewList 새로운 리스트 만들기
+                    var secondLength = secondOldList.count
+                    var secondNewList : [String] = []
+                    
+                    for _ in 0..<secondLength {
+                        let randomNumber = Int(arc4random_uniform(UInt32(secondLength)))
+                        secondNewList.append(secondOldList[randomNumber])
+                        secondOldList.remove(at: randomNumber)
+                        secondLength -= 1
+                    }
+                    
+                    print(firstNewList)
+                    print(secondNewList)
+                    
+                }
+            }
+            
+            
+        } catch {
+            print(error)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
